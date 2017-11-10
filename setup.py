4718cce77a2132d39bb66d686c2b677bbf5b163a
Alex <indifferentalex@outlook.com>
2017-11-10 18:59:20 +0100

    Rudimentary setup file for package installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name = 'botticelli',
+      version = '0.1',
+      description = 'A multi-platform Python framework for making \
+      	universal bots',
+      url = 'https://github.com/indifferentalex/botticelli',
+      author = 'indifferentalex',
+      author_email = 'indifferentalex@outlook.com',
+      license = 'MIT',
+      packages = ['botticelli'],
+      zip_safe = False)
